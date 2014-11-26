fcad897442edc7a6a0fe8b4109546a0c16cb9134
Jesse Martin <jmartin@yammer-inc.com>
2014-11-25 16:16:14 -0800

    add autoFocus directive

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,35 @@
-var campusBuildingLocator = angular.module('campusBuildingLocator', [
-  'ngRoute',
-  'campusBuildingLocatorControllers'
-]);
-
 var SEARCH_PATH = '/buildings';
-campusBuildingLocator.factory('ConfigurationService', function() {
-  return {
-    search_path: SEARCH_PATH
-  };
-});
+var campusBuildingLocator = angular
+  .module('campusBuildingLocator', [
+    'ngRoute',
+    'campusBuildingLocatorControllers'
+  ])
+  .factory('ConfigurationService', function() {
+    return {
+      search_path: SEARCH_PATH
+    };
+  })
+  .directive('autoFocus', function ($timeout) {
+    return {
+      link: {
+        post: function (scope, element, attr) {
+          element[0].focus();
+        }
+      }
+    };
+  })
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+      when(SEARCH_PATH, {
+        templateUrl: 'partials/results.html',
+        controller: 'ResultsCtrl'
+      }).
+      when(SEARCH_PATH + '/:query', {
+        templateUrl: 'partials/results.html',
+        controller: 'ResultsCtrl'
+      }).
+      otherwise({
+        redirectTo: SEARCH_PATH
+      });
+  }]);
 
-campusBuildingLocator.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when(SEARCH_PATH, {
-      templateUrl: 'partials/results.html',
-      controller: 'ResultsCtrl'
-    }).
-    when(SEARCH_PATH + '/:query', {
-      templateUrl: 'partials/results.html',
-      controller: 'ResultsCtrl'
-    }).
-    otherwise({
-      redirectTo: SEARCH_PATH
-    });
-}]);
